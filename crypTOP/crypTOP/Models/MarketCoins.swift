2a75cb69f7a6420006cc81a4d4f7809821e87ff1
Ludovic FAVIER <lfavier@live.fr>
2021-01-30 13:36:03 +0100

    error image

--- crypTOP/crypTOP/Models/MarketCoins.swift
+++ crypTOP/crypTOP/Models/MarketCoins.swift
@@ -8,10 +8,9 @@
 import Foundation
 
 class MarketCoins {
-
-    var name: String //FROMSYMBOL
-    var price: [String: Double] = [:] //TOSYMBOL + PRICE
-    var srcImage: String? //IMAGEURL
+    private var name: String //FROMSYMBOL
+    private var price: [String: Double] = [:] //TOSYMBOL + PRICE
+    private var srcImage: String? //IMAGEURL
 
 
     init(name: String) {
@@ -23,39 +22,91 @@ class MarketCoins {
     public func setSrcImage(srcImage: String){
         self.srcImage = srcImage
     }
+    public func getName() -> String{
+        return self.name
+    }
+    public func getImage() -> String{
+        return self.srcImage!
+    }
 
 }
 
 extension MarketCoins{
+    public static func createMarketCoins(jsonObject: Any, marketCoins: inout MarketCoins, parameters: [String], currencies: [String]) {
+        if let array = jsonObject as? [Any] {
+            array.forEach {
+                createMarketCoins(jsonObject: $0, marketCoins: &marketCoins, parameters: parameters, currencies: currencies)
+            }
+        }
+        else if let dict = jsonObject as? [String : Any] {
+            for key in dict.keys {
+                if currencies.contains(key) {
+                    //marketsCoins.append(dict[key] as! String)
+                    if let currency = dict[key] as? [String: Any] {
+                        guard let price = currency["PRICE"] as? Double, let cur = currency["TOSYMBOL"] as? String else{
+                            print("Error in searching price or cur in currency:" + key)
+                            return
+                        }
+                        guard let imageUrl = currency["IMAGEURL"] as? String else{
+                            print("Error in searching imageUrl in currency:" + key)
+                            return
+                        }
+                        marketCoins.addPrice(currency: cur, price: price)
+                        marketCoins.setSrcImage(srcImage: "cryptocompare.com" + imageUrl)
+                    }
+                    print(key)
+                }
+                else {
+                    createMarketCoins(jsonObject: dict[key]!, marketCoins: &marketCoins, parameters: parameters, currencies: currencies)
+                }
+            }
+        }
+    }
+    public static func extractMarketsCoins(jsonObject: Any, marketsCoins: inout [MarketCoins], parameters: [String], currencies: [String]) {
+        if let array = jsonObject as? [Any] {
+            array.forEach {
+                extractMarketsCoins(jsonObject: $0, marketsCoins: &marketsCoins, parameters: parameters, currencies: currencies)
+            }
+        }
+        else if let dict = jsonObject as? [String : Any] {
+            for key in dict.keys {
+                if parameters.contains(key) {
+                    //marketsCoins.append(dict[key] as! String)
+                    marketsCoins.append(MarketCoins(name: key))
+                    guard var lastMarket = marketsCoins.last else{
+                        print("Error in creating a marketCoins")
+                        return
+                    }
+                    createMarketCoins(jsonObject: dict[key]!, marketCoins: &lastMarket, parameters: parameters, currencies: currencies)
+                    print(key)
+                }
+                else {
+                    extractMarketsCoins(jsonObject: dict[key]!, marketsCoins: &marketsCoins, parameters: parameters, currencies: currencies)
+                }
+            }
+        }
+    }
+    public static func URLRequest(urlString: String, completion: @escaping ([MarketCoins]) -> Void){
+        let url = URL(string: urlString)!
+        URLSession.shared.dataTask(with: url, completionHandler: {(data, response, error) -> Void in
+            if let data = data{
+                do{
+                    if let json =  try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]{
+                        if let raw = json["RAW"] as? [String: Any] {
+                            var marketsCoins = [MarketCoins]()
+                            let parameters = ["BTC", "ETH"]
+                            let currencies = ["USD", "EUR"]
+                            self.extractMarketsCoins(jsonObject: raw, marketsCoins: &marketsCoins, parameters: parameters, currencies: currencies)
+                            completion(marketsCoins)
+                        }
+                    }
+                }
+                catch{
+                    print(error)
+                }
+            }
+        })
+        .resume()
+    }
 
-//    static func initMarket(urlString: String, completion: ([MarketCoins]) -> Void) {
-//        // Create a URLRequest for API Cryptocompare (BTC as coin, USD and EUR as cryptocurrencies)
-//        let url = URL(string: urlString)!
-//        let task = URLSession.shared.dataTask(with: url, ) { data, response, error in
-//            if let data = data{
-//                do{
-//                    if let json =  try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]{
-//                    //if let root = json as? [String: Any] {
-//
-//                    }
-//
-//                    //print(json)
-//
-////                    DispatchQueue.main.async {
-////                        self.priceLabel.text = json
-////                    }
-//                } catch{
-//                    print(error)
-//                }
-//            }
-////            if let error = error {
-////                return
-////            }
-//            guard let httpResponse = response as? HTTPURLResponse,
-//                (200...299).contains(httpResponse.statusCode) else {
-//                return
-//            }
-//        }
-//        task.resume()
-//    }
 }
--- crypTOP/crypTOP/Scenes/HomeViewController.swift
+++ crypTOP/crypTOP/Scenes/HomeViewController.swift
@@ -8,103 +8,67 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    private let currency = ["USD", "EUR"]
-    //private var markets: [MarketCoins]
+    
+    public var markets: [MarketCoins]? = nil
+
     @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var imageLabel: UIImageView!
+    
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
         //appel unique de la fonction iniMarkets
-        _ = initMarkets
+        print("ViewWillAppear")
+        dump(self.markets)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.URLRequest()
+        print("ViewDidLoad")
+        _ = initMarkets
+        dump(self.markets)
+        //self.priceLabel.text = self.markets?[0].getName()
 
         // Do any additional setup after loading the view.
     }
     //Appeler qu'une seule fois la fonction pour initialiser des markets
     private lazy var initMarkets: Void = {
+        var markets = [MarketCoins]()
         let fsyms = "BTC,ETH"
         let tsyms = "USD,EUR"
         let apiKey = "8cf7e8afc81e05ac24dff8fde7c369a53cfb6820fd77245245dffb9762fd9c90"
         let urlString = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + fsyms + "&tsyms=" + tsyms + "&api_key=" + apiKey
-        URLRequest(urlString: urlString)
+        MarketCoins.URLRequest(urlString: urlString,completion:{ marketCoins in
+            DispatchQueue.main.async {
+                self.markets = marketCoins
+                print("InitMarket")
+                dump(self.markets)
+                self.priceLabel.text = marketCoins[0].getName()
+                self.imageLabel.prepareDownLoad(from: marketCoins[0].getImage())
+            }
+        })
+        self.markets = markets
+
 
     }()
-    func createMarketCoins(jsonObject: Any, marketCoins: inout MarketCoins, parameters: [String]) {
-        if let array = jsonObject as? [Any] {
-            array.forEach {
-                createMarketCoins(jsonObject: $0, marketCoins: &marketCoins, parameters: parameters)
-            }
-        }
-        else if let dict = jsonObject as? [String : Any] {
-            for key in dict.keys {
-                if self.currency.contains(key) {
-                    //marketsCoins.append(dict[key] as! String)
-                    if let currency = dict[key] as? [String: Any] {
-                        guard let price = currency["PRICE"] as? Double, let cur = currency["TOSYMBOL"] as? String else{
-                            print("Error in searching price or cur in currency:" + key)
-                            return
-                        }
-                        guard let imageUrl = currency["IMAGEURL"] as? String else{
-                            print("Error in searching imageUrl in currency:" + key)
-                            return
-                        }
-                        marketCoins.addPrice(currency: cur, price: price)
-                        marketCoins.setSrcImage(srcImage: imageUrl)
-                    }
-                    print(key)
-                }
-                else {
-                    createMarketCoins(jsonObject: dict[key]!, marketCoins: &marketCoins, parameters: parameters)
-                }
-            }
-        }
-    }
-    func extractMarketsCoins(jsonObject: Any, marketsCoins: inout [MarketCoins], parameters: [String]) {
-        if let array = jsonObject as? [Any] {
-            array.forEach {
-                extractMarketsCoins(jsonObject: $0, marketsCoins: &marketsCoins, parameters: parameters)
-            }
-        }
-        else if let dict = jsonObject as? [String : Any] {
-            for key in dict.keys {
-                if parameters.contains(key) {
-                    //marketsCoins.append(dict[key] as! String)
-                    marketsCoins.append(MarketCoins(name: key))
-                    guard var lastMarket = marketsCoins.last else{
-                        print("Error in creating a marketCoins")
-                        return
-                    }
-                    createMarketCoins(jsonObject: dict[key]!, marketCoins: &lastMarket, parameters: parameters)
-                    print(key)
-                }
-                else {
-                    extractMarketsCoins(jsonObject: dict[key]!, marketsCoins: &marketsCoins, parameters: parameters)
-                }
-            }
-        }
-    }
-    func URLRequest(urlString: String){
-        let url = URL(string: urlString)!
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data{
-                do{
-                    if let json =  try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]{
-                        if let raw = json["RAW"] as? [String: Any] {
-                            var marketsCoins = [MarketCoins]()
-                            let parameters = ["BTC", "ETH"]
-                            self.extractMarketsCoins(jsonObject: raw, marketsCoins: &marketsCoins, parameters: parameters)
-                        }
-                    }
-                }
-                catch{
-                    print(error)
-                }
-            }
-        }
-        task.resume()
-    }
     
 
+}
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    func prepareDownLoad(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
 }
